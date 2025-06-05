263216eb1413a3e551805d16e20589e3a279c1af
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-06-05 19:09:21 +0500

    Conversation Routes Setup

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import connectdb from './database/connection.js';
 import ticketRoutes from './routes/ticketRoutes.js';
 import subscriptionRoutes from './routes/subscriptionRoutes.js';
 import faqRoutes from './routes/faqRoutes.js';
-// import conversationRoutes from './routes/conversationRoutes.js';
+import conversationRoutes from './routes/conversationRoutes.js';
 
 const app = express();
 
@@ -16,7 +16,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/tickets', ticketRoutes);
 app.use('/api/subscriptions', subscriptionRoutes);
 app.use('/api/faqs', faqRoutes);
-// app.use('/api/conversations', conversationRoutes);
+app.use('/api/conversations', conversationRoutes);
 
 const PORT = process.env.PORT || 8000;
 
