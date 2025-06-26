9a639f26d098ec06f637775fb78a000fb3d7a99f
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-06-26 20:21:47 +0500

    Update cloudinary.js

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -2,6 +2,7 @@ import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 import "dotenv/config";
 
+// Cloudinary Config Getting From an env file
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
